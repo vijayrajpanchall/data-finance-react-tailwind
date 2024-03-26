aa1ad055c14ff66d55bfab3235907ab58a6dde29
vijayrajpanchall <vijayrajpanchal4@gmail.com>
2024-03-26 14:44:11 +0530

    adds newsletter, footer and cards components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Newsletter from "./components/Newsletter";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+import Analytics from "./components/Analytics";
 
 function App() {
   return (
     <div>
-     <Navbar/>
+      <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
     </div>
   );
 }
